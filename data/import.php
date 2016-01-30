91315dc3ab51e03c1e2815ce8844ad41b56f7ef4
degeawapsh <degeawapsh@gmail.com>
2016-01-30 22:18:44 +0700

    version 1.0

---
+++ data/import.php
@@ -0,0 +1,37 @@
+<?php
+$sql = new mysqli("asus", "root", "root", "mydb");
+if(mysqli_connect_error()){
+	die(mysqli_connect_error());
+}
+if($sql->query("TRUNCATE TABLE bible") === FALSE){
+	echo "SQL Error";
+}
+
+$s = file_get_contents("./list.txt");
+$s = split("\n", $s);
+$id = 1;
+
+foreach($s as $f){
+	$a = file_get_contents($f.".txt");
+	$a = split("\n", $a);
+	$t = '';
+	$doan = 1;
+	foreach($a as $i){
+		if(preg_match('/^[0-9]+$/', $i)==1 && $i==$doan+1){
+			$sql->query("INSERT INTO bible (id, data) VALUE (".$id.", '".$t."');");
+			echo $sql->error;
+			$t = $i;
+			$id++;
+			$doan++;
+		}else{
+			$t=$t."<BR/>".$i;
+		}
+	}
+	$sql->query("INSERT INTO bible (id, data) VALUE (".$id.", '".$t."');");
+	echo $sql->error;
+	$id++;
+	echo "finish ".$f."\n";
+}
+
+$sql->close();
+?>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Mỗi ngày một đoạn Kinh Thánh</title>
+  <link rel="stylesheet" type="text/css" href="/css.css"/>
+</head>
+<body>
+<?php
+$sql = new mysqli("asus", "root", "root", "mydb");
+if(mysqli_connect_error()){
+	die(mysqli_connect_error());
+}
+$sql->set_charset("utf8");
+$id = 178+round((time()-1440226702)/86400-0.5);
+
+$res = $sql->query("SELECT data FROM bible WHERE id=".$id);
+$row = $res->fetch_row();
+echo $row[0];
+
+$sql->close();
+?>
+</body>
+</html>
