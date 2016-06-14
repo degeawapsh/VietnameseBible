be79bfc11b03eb90618ee37808ee7641bb3fd1d7
dhad05 <degeawapsh@gmail.com>
2016-06-14 23:31:01 +0700

    add book name to title

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Mỗi ngày một đoạn Kinh Thánh</title>
-  <link rel="stylesheet" type="text/css" href="/css.css"/>
-</head>
-<body>
-<?php
 $sql = new mysqli("asus.ml", "root", "root", "mydb");
 if(mysqli_connect_error()){
 	die(mysqli_connect_error());
@@ -17,11 +7,24 @@ if(mysqli_connect_error()){
 $sql->set_charset("utf8");
 $id = 178+round((time()-1440226702)/86400-0.5);
 
-$res = $sql->query("SELECT data FROM bible WHERE id=".$id);
-$row = $res->fetch_row();
-echo $row[0];
-
+$res = $sql->query("SELECT `data` FROM `bible` WHERE `id`=".$id);
+$data = $res->fetch_row();
+$res = $sql->query("SELECT `title` FROM `bible` WHERE `id`=".$id);
+$title = $res->fetch_row();
 $sql->close();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title><?php
+        echo $title[0];
+    ?></title>
+  <link rel="stylesheet" type="text/css" href="/css.css"/>
+</head>
+<body>
+<?php
+echo $data[0];
 ?>
 </body>
 </html>
